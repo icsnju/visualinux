dd31b879e02c392d13251442e8ea0cc26c35cd12
nan <mu.nan.11@outlook.com>
2024-08-30 22:11:11 +0800

    fix: 修复非lts的node版本约束

--- scripts/build/node_install.sh
+++ scripts/build/node_install.sh
@@ -7,21 +7,21 @@ fi
 
 version_cur="$(node -v)"
 version_cur="${version_cur:1}"
-version_req="18.0.0"
+version_req="20.17.0"
 
-if [ "$(printf '%s\n' "$version_req" "$version_cur" | sort -V | head -n1)" = "$version_req" ]; then 
+if [ "$(printf '%s\n' "$version_req" "$version_cur" | sort -V | head -n1)" = "$version_req" ]; then
     echo "node version check passed: $version_cur > $version_req"
     npm install
     exit 0
 fi
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
 
 # to use nvm now
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 
 export NVM_NODEJS_ORG_MIRROR=https://npmmirror.com/mirrors/node
-nvm install 18
+nvm install 20
 npm install
